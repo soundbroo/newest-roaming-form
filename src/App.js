41496592be2a40e40be25855a73efe22b7bdbbfc
Станкевич Илья Сергеевич <stankevich_is@astralnalog.ru>
2020-03-16 14:50:41 +0300

    Заглушка для ie и edge

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Bowser from "bowser";
 
 import Content from "components/Content";
+import InvalidBrowser from "components/Common/InvalidBrowser";
 
-const App = () => <Content />;
+const App = () => {
+  const browser = Bowser.getParser(window.navigator.userAgent).getBrowserName();
+  switch (browser) {
+    case "Internet Explorer":
+    case "Microsoft Edge":
+      return <InvalidBrowser browser={browser} />;
+  }
+  return <Content />;
+};
 
 export default App;
---
+++ src/components/Common/InvalidBrowser.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+const InvalidBrowser = ({ browser }) => (
+  <Warning>
+    <div>Браузер {browser} устарел.</div>
+    <div>
+      Вы пользуетесь устаревшим браузером, который не поддерживает современные
+      веб-стандарты и представляет угрозу вашей безопасности. Пожалуйста,
+      установите современный браузер.
+    </div>
+  </Warning>
+);
+
+export default InvalidBrowser;
+
+const Warning = styled.div`
+  border: 1px solid #cecece;
+  position: absolute;
+  left: 50%;
+  top: 20%;
+  transform: translateX(-50%) translateY(-50%);
+  border-radius: 6px;
+  padding: 12px;
+  div {
+    margin: 6px 0;
+  }
+`;
