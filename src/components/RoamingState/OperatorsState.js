758e2aced4e2b5bb270ea40a06356298f29009ec
soundbroo <soundbroo@gmail.com>
2020-04-30 14:18:46 +0300

    Обновил таблицу состояния роуминга

--- src/components/RoamingState/OperatorsState.js
+++ src/components/RoamingState/OperatorsState.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled, { css } from "styled-components";
 import {
   Table,
@@ -11,18 +11,19 @@ import {
   LinearProgress,
   TextField,
   MenuItem,
-  Tooltip
+  Tooltip,
+  Avatar,
 } from "@material-ui/core";
 import FindInPageRoundedIcon from "@material-ui/icons/FindInPageRounded";
-
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
 import {
   CATEGORIES,
   OPERATOR_STATE_MESSAGES,
-  OPERATOR_STATE_DATA
+  OPERATOR_STATE_DATA,
 } from "constants";
 
 const Search = ({ search, setSearch }) => {
-  const handleSearch = event => {
+  const handleSearch = (event) => {
     setSearch(event.target.value);
   };
   return (
@@ -33,14 +34,14 @@ const Search = ({ search, setSearch }) => {
       autoFocus
       value={search}
       onChange={handleSearch}
-      onBlur={e => e.preventDefault()}
+      onBlur={(e) => e.preventDefault()}
       variant="outlined"
     />
   );
 };
 
 const Select = ({ category, setCategory }) => {
-  const handleChange = event => {
+  const handleChange = (event) => {
     setCategory(event.target.value);
   };
   return (
@@ -51,7 +52,7 @@ const Select = ({ category, setCategory }) => {
       onChange={handleChange}
       variant="outlined"
     >
-      {Object.values(CATEGORIES).map(value => (
+      {Object.values(CATEGORIES).map((value) => (
         <MenuItem key={value} value={value}>
           {value}
         </MenuItem>
@@ -63,24 +64,41 @@ const Select = ({ category, setCategory }) => {
 const OperatorsState = () => {
   const [category, setCategory] = useState(CATEGORIES.all);
   const [search, setSearch] = useState("");
+  const [sortedByInvitation, setSortedByInvitation] = useState(false);
+  const [filteredRows, setFilteredRows] = useState(OPERATOR_STATE_DATA);
 
-  const filteredRows = OPERATOR_STATE_DATA.filter(row =>
-    row.name.toUpperCase().includes(search.toUpperCase())
-  ).filter(row => {
-    if (category === CATEGORIES.all) {
-      return row;
-    } else return row.cat === category;
-  });
+  useEffect(() => {
+    sortedByInvitation
+      ? setFilteredRows(sortByInvitetion())
+      : setFilteredRows(filterRows());
+  }, [sortedByInvitation, category, search]);
+
+  const sortByInvitetion = () => filterRows().filter((row) => row.invitation);
+
+  const filterRows = () =>
+    OPERATOR_STATE_DATA.filter((row) =>
+      row.name.toUpperCase().includes(search.toUpperCase())
+    ).filter((row) => {
+      if (category === CATEGORIES.all) {
+        return row;
+      } else return row.cat === category;
+    });
 
   const renderRows = () => {
-    if (filteredRows.length) {
-      return filteredRows.map(row => (
-        <TableRow key={row.name}>
+    if (filteredRows?.length) {
+      return filteredRows.map((row) => (
+        <Row key={row.name}>
           <LabelCell component="th" scope="row">
             {row.name}
           </LabelCell>
+          <Cell align="left">{row.prefix}</Cell>
           <Cell catcell="true" align="left">
             {row.cat}
+          </Cell>
+          <Cell align="left">
+            {row.invitation && (
+              <PeopleAltIcon color="primary" fontSize="default" />
+            )}
           </Cell>
           <Tooltip title={`${row.status}%`} placement="left">
             <Cell align="right">
@@ -91,32 +109,44 @@ const OperatorsState = () => {
               />
             </Cell>
           </Tooltip>
-        </TableRow>
+        </Row>
       ));
     }
     return (
-      <TableRow key="empty">
-        <Cell colSpan={3}>
+      <Row key="empty">
+        <Cell colSpan={5}>
           <Nothing>
             <FindInPageRoundedIcon />
             <div>{OPERATOR_STATE_MESSAGES.notFound}</div>
           </Nothing>
         </Cell>
-      </TableRow>
+      </Row>
     );
   };
 
   return (
     <Container component={Paper}>
       <Table>
-        <TableRow>
-          <HeadCell colSpan={2}>
-            <Search search={search} setSearch={setSearch} />
-          </HeadCell>
-          <SelectCell colSpan={1} align="left">
-            <Select category={category} setCategory={setCategory} />
-          </SelectCell>
-        </TableRow>
+        <TableHead>
+          <Row>
+            <Cell colSpan={3}>
+              <Search search={search} setSearch={setSearch} />
+            </Cell>
+            <AvatarCell
+              align="left"
+              onClick={() => setSortedByInvitation(!sortedByInvitation)}
+            >
+              <Tooltip title="Поддерживается отправка приглашений">
+                <Invitation active={sortedByInvitation}>
+                  <PeopleAltIcon color="primary" />
+                </Invitation>
+              </Tooltip>
+            </AvatarCell>
+            <SelectCell colSpan={1} align="left">
+              <Select category={category} setCategory={setCategory} />
+            </SelectCell>
+          </Row>
+        </TableHead>
         <TableBody>{renderRows()}</TableBody>
       </Table>
     </Container>
@@ -126,22 +156,47 @@ const OperatorsState = () => {
 export default OperatorsState;
 
 const Container = styled(TableContainer)`
-  max-width: 1000px !important;
+  max-width: 1200px !important;
   width: 100%;
   overflow-x: hidden !important;
+  &::-webkit-scrollbar {
+    width: 6px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: #c5c5c5;
+  }
   @media (max-width: 660px) {
     margin-top: 9px;
   }
 `;
 
+const Row = styled(TableRow)`
+  height: 48px !important;
+`;
+
 const Cell = styled(TableCell)`
+  padding: 0 16px !important;
   @media (max-width: 660px) {
-    ${p =>
+    padding: 0 3px !important;
+    height: 48px;
+    ${(p) =>
       p.catcell &&
       css`
         font-size: 0 !important;
         padding: 0 !important;
       `}
+  }
+`;
+
+const Invitation = styled(Avatar)`
+  background-color: ${({ active }) =>
+    active ? "#6a1b9a !important" : "#fff !important"};
+  border: 1.3px solid #6a1b9a;
+  width: 32px !important;
+  height: 32px !important;
+  cursor: pointer;
+  svg {
+    color: ${({ active }) => (active ? "#fff" : "#6a1b9a")};
   }
 `;
 
@@ -156,8 +211,14 @@ const Input = styled(TextField)`
 `;
 
 const HeadCell = styled(TableCell)`
-  padding: 6px !important;
   width: 200px;
+  @media (max-width: 660px) {
+    padding: 0 3px !important;
+  }
+`;
+
+const AvatarCell = styled(Cell)`
+  width: 36px;
 `;
 
 const SelectCell = styled(HeadCell)`
@@ -166,7 +227,7 @@ const SelectCell = styled(HeadCell)`
     text-overflow: ellipsis;
     white-space: nowrap;
     width: inherit;
-    max-width: 140px;
+    max-width: 86px;
   }
 `;
 
@@ -184,19 +245,20 @@ const Nothing = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  padding: 60px 0;
   svg {
     font-size: 165px;
-    color: ${p => p.theme.palette.grey};
+    color: ${(p) => p.theme.palette.grey};
   }
   div {
-    color: ${p => p.theme.palette.darkGrey};
+    color: ${(p) => p.theme.palette.darkGrey};
     font-weight: 600;
     letter-spacing: 0.05em;
   }
 `;
 
 const Progress = styled(LinearProgress)`
-  background: ${p => {
+  background: ${(p) => {
     switch (p.value) {
       case 100:
         return "#4caf50";
@@ -210,7 +272,7 @@ const Progress = styled(LinearProgress)`
   }} !important;
 
   div {
-    background: ${p => {
+    background: ${(p) => {
       switch (p.value) {
         case 100:
           return "#4caf50";
